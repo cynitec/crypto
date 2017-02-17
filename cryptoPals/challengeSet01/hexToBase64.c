03431acfcfabee032ecaa0b6e7f1c7f138d3d934
craig <sgccarey@gmail.com>
2017-02-17 15:57:27 +0000

    converting first 6 bytes of an int to a char

--- cryptoPals/challengeSet01/hexToBase64.c
+++ cryptoPals/challengeSet01/hexToBase64.c
@@ -1,33 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-
-// assumes little endian
-void printBits(size_t const size, void const * const ptr)
-{
-    unsigned char *b = (unsigned char*) ptr;
-    unsigned char byte;
-
-    for (int i = size-1; i >= 0; i--)
-    {
-        for (int j = 7; j >= 0; j--)
-        {
-            byte = (b[i] >> j) & 1;
-            printf("%u", byte);
-        }
-    }
-    puts("");
-}
 
 int main(int argc, char *argv[])
 {
-    int i = 23;
-    uint ui = UINT_MAX;
-    int iMin = INT_MIN;
-    float f = 23.45f;
-    printBits(sizeof(i), &i);
-    printBits(sizeof(ui), &ui);
-    printBits(sizeof(f), &f);
-    printBits(sizeof(iMin), &iMin);
-    return 0;
+    int x = 13;     // 1101
+
+    unsigned char byte = 0;
+
+    byte = (x&1) + (x&2) + (x&4) + (x&8) + (x&16) + (x&32);
+
+    printf("%u\n", byte);
 }
